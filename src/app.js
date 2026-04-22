29df476c539b6da7ae55c3abfcb483c865c41fe0
mubarek <mezid58@gmail.com>
2026-04-22 17:07:06 +0300

    updated

--- src/app.js
+++ src/app.js
@@ -1,10 +1,17 @@
 const express = require('express');
+const cors = require('cors');
+const cookieParser = require('cookie-parser');
 const routes = require('./routes');
 const { errorHandler, notFound } = require('./middleware/errorHandler');
 const { setupSwagger } = require('./docs/swagger');
 
 const app = express();
 
+app.use(cors({
+  origin: true,          // Allow all origins
+  credentials: true,     // Allow cookies/auth headers
+}));
+app.use(cookieParser());
 app.use(express.json());
 setupSwagger(app);
 
